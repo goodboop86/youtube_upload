d9a0a2dc3a405cde15fb6432d308d0e6747439e5
nari-suke <goodboop86@gmail.com>
2022-03-05 13:36:47 +0900

    refactor

--- job/directory_create.py
+++ job/directory_create.py
@@ -2,27 +2,30 @@
 # localからでも参照できるが、せっかくなのでAPIを使ってみる
 from datetime import datetime, timedelta
 from prefect import task
-from prefect import config as pconf
+from prefect import config as pconfig
 
 from prefect.utilities.notifications import slack_notifier
 
 
 @task(name="directory_create", state_handlers=[slack_notifier])
-def directory_create(client, conf) -> int:
-    query = conf.get("GET_FNAME_FROMDIR_ID").replace("[DIR_ID]", pconf.context.drive.YOUTUBE_DIR_ID)
+def directory_create(client, config) -> int:
+    # ディレクトリ情報を取得
+    query = config.query["GET_FNAME_FROMDIR_ID"].replace("[DIR_ID]", pconfig.context.drive.YOUTUBE_DIR_ID)
+    result = client.drive.files().list(q=query, pageSize=20,
+                                       fields=config.query["FIELD1"]).execute().get('files', [])
 
-    result = client.files().list(q=query, pageSize=20,
-                                 fields=conf.get("FIELD1")).execute().get('files', [])
-
+    # 存在しているディレクトリを取得
     dirs = [elem["name"] for elem in result if
             elem['mimeType'] == 'application/vnd.google-apps.folder' and elem['trashed'] is False]
 
+    # ディレクトリ情報
     file_metadata = {
         'name': '',
         'mimeType': 'application/vnd.google-apps.folder',
-        'parents': [pconf.context.drive.YOUTUBE_DIR_ID]
+        'parents': [pconfig.context.drive.YOUTUBE_DIR_ID]
     }
 
+    # 向こう７日分を作成
     for i in range(7):
         date = datetime.now() + timedelta(days=i)
         date_str = date.strftime("%Y-%m-%d")
--- job/get_upload_file.py
+++ job/get_upload_file.py
@@ -6,11 +6,11 @@ from prefect import config as pconf
 
 
 @task(name="get_upload_info", state_handlers=[slack_notifier])
-def get_upload_info(client, config, status):
+def get_upload_file(client, config, status):
     def get_params_from_sheet_by_date(df, _date) -> dict:
         return df[df.date == _date].to_dict("records")
 
-    def create_youtube_request_param(_params, _config):
+    def create_youtube_request_param(_params, _conf):
         _file = "{}{}/{}".format(pconf.context.ABSOLUTE_DRIVE_PATH, _params["date"], _params["file"])
         privacy_status = {"privacyStatus": _params["privacyStatus"]}
         del _params['file'], _params['date'], _params['privacyStatus']
@@ -19,14 +19,19 @@ def get_upload_info(client, config, status):
 
     # spreadシートからtsv中身を取得
     header = client.spread.spreadsheets().values().get(spreadsheetId=pconf.context.spread.UPLOAD_LIST_ID,
-                                                       range=config.spread["HEADER_RANGE"]).execute().get('values', [])
+                                                       range=config.spread.get("HEADER_RANGE")).execute().get('values', [])
+
     body = client.spread.spreadsheets().values().get(spreadsheetId=pconf.context.spread.UPLOAD_LIST_ID,
-                                                     range=config.spread["BODY_RANGE"]).execute().get('values', [])
+                                                     range=config.spread.get("BODY_RANGE")).execute().get('values', [])
+
+    query = config.query.get("GET_FNAME_FROMDIR_ID").replace("[DIR_ID]", pconf.context.drive.YOUTUBE_DIR_ID)
+    result = client.drive.files().list(q=query, pageSize=20,
+                                 fields=config.query.get("FIELD1")).execute().get('files', [])
 
     params = get_params_from_sheet_by_date(
         df=pd.DataFrame(body, columns=header[0]),
         _date=datetime.now().strftime("%Y-%m-%d"))[0]
 
-    file, body = create_youtube_request_param(params, config)
+    file, body = create_youtube_request_param(params, config.youtube)
 
     return [file, body]
