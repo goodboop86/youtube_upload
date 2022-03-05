a2ee573cd30e2e19a5a0c8aa57bfb616465fd791
nari-suke <goodboop86@gmail.com>
2022-03-05 16:49:34 +0900

    refactor

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
 import workflow.youtube_upload_from_gdrive as wf
+
+from model.config import Config
+from model.api_client import ApiClient
+from conf import conf
+from util.util import get_client
+
 app = FastAPI()
 
+config = Config(drive=conf.drive_conf,
+                spread=conf.spread_conf,
+                youtube=conf.youtube_conf,
+                query=conf.query_conf)
 
-@app.get("/youtube_upload_workflow")
+client = ApiClient(drive=get_client(config.drive),
+                   spread=get_client(config.spread),
+                   youtube=get_client(config.youtube))
+
+
+@app.get("/youtube_upload_from_gdrive")
 async def root():
-    state = wf.youtube_upload_from_gdrive()
+    state = wf.youtube_upload_from_gdrive(client=client, config=config)
     return {"message": f"{state.result}"}
