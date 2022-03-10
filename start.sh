a545c102d0ed961342f438f795cd912027e3e3ef
nari-suke <goodboop86@gmail.com>
2022-03-10 20:14:43 +0900

    refactor

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ set -e
 export PREFECT__LOGGING__LEVEL="INFO"
 export PREFECT__LOGGING__FORMAT="[%(asctime)s] %(levelname)s - %(name)s | %(message)s"
 
+echo $
 
 prefect agent local start &
 uvicorn main:app --host 0.0.0.0 --port 8080 &
